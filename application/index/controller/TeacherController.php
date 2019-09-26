49389c561d344fcda4f4358aff6d7f925ee97b4f
zhaokaiqiangZzz <2391945695@qq.com>
2019-09-26 22:39:30 +0800

    0.701

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -76,8 +76,9 @@ class TeacherController extends TIndexController
         $terms = Term::all();
         $this->assign('terms', $terms);
         
-        $teachers = Teacher::all();
-        $this->assign('teachers', $teachers);
+        $teacherId = session('teacherId');
+        $teacher = Teacher::get($teacherId);
+        $this->assign('teacher', $teacher);
         $klass = new Klass;
         $this->assign('Klasses', $klass->select());
         $this->assign('Course', new Course);
@@ -99,7 +100,7 @@ class TeacherController extends TIndexController
 
         // 验证
         if (!$Course->save()) {
-            return $this->error('保存错误：' . $Course->getError());
+            return $this->error('保存错误1：' . $Course->getError());
         }
 
         //--------------------增加班级课程信息------------
@@ -135,8 +136,9 @@ class TeacherController extends TIndexController
         
         $terms = Term::all();
         $this->assign('terms', $terms);
-        $teachers = Teacher::all();
-        $this->assign('teachers', $teachers);
+        $teacherId = session('teacherId');
+        $teacher = Teacher::get($teacherId);
+        $this->assign('teacher', $teacher);
         $klass = new Klass;
         $this->assign('Klasses', $klass->select());
 
@@ -158,7 +160,8 @@ class TeacherController extends TIndexController
         $Course->term_id = $this->request->param('term_id/d');
         $Course->teacher_id = $this->request->param('teacher_id/d');
         $Course->type = $this->request->param('type/d');
-        if (is_null($Course->save())) {
+        
+        if (!$Course->save()) {
             return $this->error('课程信息更新发生错误：' . $Course->getError());
         }
 
@@ -679,9 +682,10 @@ class TeacherController extends TIndexController
     {
         $scores = $this->request->post();
         $key = $this->request->post('key');
-        //dump ($key);
-        //dump ($scores);
-        //dump ($scores["id"]["0"]);
+        dump ($key);
+        dump ($scores);
+        dump ($scores["id"]["0"]);
+        return ;
         $message = '更新成功';
         for ($i=0; $i < $key - 1; $i++) { 
             $score = score::get($scores["id"][$i]);
